7355b35f01699b244382bf6ed3b60e76ba08c68a
yangyue <1569293449@qq.com>
2018-11-06 23:00:51 +0800

    单例模式

---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private Singleton() { }
+        private static readonly object locker = new object();
+        private static Singleton instance;
+        public static Singleton GetInstance()
+        {
+            if(instance==null)
+            {
+                lock(locker)
+                {
+                    if(instance==null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
