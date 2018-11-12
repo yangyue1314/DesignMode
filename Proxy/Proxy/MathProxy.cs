ec7fd1f4304c8c65d494ff3b00693bd7f8421947
yangyue <1569293449@qq.com>
2018-11-12 22:43:34 +0800

    命令模式

---
+++ Proxy/Proxy/MathProxy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+    class MathProxy
+    {
+        private Math math = new Math();
+
+        public double Add(double x, double y)
+        {
+            return math.Add(x, y);
+        }
+        public double Sub(double x, double y)
+        {
+            return math.Sub(x, y);
+        }
+        public double Mul(double x, double y)
+        {
+            return math.Mul(x, y);
+        }
+        public double Div(double x, double y)
+        {
+            return math.Div(x, y);
+        }
+    }
+}
